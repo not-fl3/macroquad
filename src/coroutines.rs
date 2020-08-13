2608486c136996a47507ae6c2dab03db2bd09f4d
Fedor Logachev <not.fl3@gmail.com>
2020-08-12 20:15:08 -0500

    coroutins: tweens
    Special coroutines to change some data over time.

--- src/coroutines.rs
+++ src/coroutines.rs
@@ -26,8 +26,7 @@ impl CoroutinesContext {
     pub fn update(&mut self) {
         for future in &mut self.futures {
             if let Some((f, context)) = future {
-                let futures_context_ref: &mut _ =
-                    unsafe { std::mem::transmute(context) };
+                let futures_context_ref: &mut _ = unsafe { std::mem::transmute(context) };
 
                 if matches!(f.as_mut().poll(futures_context_ref), Poll::Ready(_)) {
                     *future = None;
@@ -60,6 +59,12 @@ pub unsafe fn start_coroutine(future: impl Future<Output = ()>) -> Coroutine {
     }
 }
 
+pub unsafe fn stop_all_coroutines() {
+   let context = &mut get_context().coroutines_context;
+
+    context.futures.clear();
+}
+
 pub fn stop_coroutine(coroutine: Coroutine) {
     let context = &mut get_context().coroutines_context;
 
@@ -88,5 +93,45 @@ pub fn wait_seconds(time: f32) -> TimerDelayFuture {
     TimerDelayFuture {
         start_time: miniquad::date::now(),
         time,
+    }
+}
+
+pub mod tweens {
+    use std::future::Future;
+    use std::pin::Pin;
+    use std::{ops::{Sub, Add, Mul}, task::{Context, Poll}};
+
+    pub struct LinearTweanFuture<T> where T: Copy + Add<Output = T> + Sub<Output = T> + Mul<f32, Output = T> {
+        var: *mut T,
+        from: T,
+        to: T,
+        start_time: f64,
+        time: f32,
+    }
+    impl<T> Unpin for LinearTweanFuture<T> where T: Copy + Add<Output = T> + Sub<Output = T> + Mul<f32, Output = T> {}
+
+    impl<T> Future for LinearTweanFuture<T> where T: Copy + Add<Output = T> + Sub<Output = T> + Mul<f32, Output = T> {
+        type Output = Option<()>;
+
+        fn poll(mut self: Pin<&mut Self>, _: &mut Context) -> Poll<Self::Output> {
+            let t = (miniquad::date::now() - self.start_time) / self.time as f64;
+            if t <= 1. {
+                unsafe { *self.var = self.from + (self.to - self.from) * t as f32 };
+                Poll::Pending
+            } else {
+                unsafe { *self.var = self.to };
+                Poll::Ready(Some(()))
+            }
+        }
+    }
+
+    pub fn linear<T>(x: &mut T, to: T, time: f32) -> LinearTweanFuture<T> where T: Copy + Add<Output = T> + Sub<Output = T> + Mul<f32, Output = T> {
+        LinearTweanFuture {
+            var: x as *mut _,
+            to,
+            from: *x,
+            time,
+            start_time: miniquad::date::now()
+        }
     }
 }
