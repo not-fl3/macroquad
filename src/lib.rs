8a205de574b003683e1d23af5459acf72ad962ba
Cedric Hutchings <cedhut02@gmail.com>
2020-08-31 18:39:17 -0400

    Add load_file and use it in load_texture

--- src/lib.rs
+++ src/lib.rs
@@ -483,3 +483,22 @@ pub fn gl_use_default_pipeline() {
 
     context.gl.pipeline(None);
 }
+
+pub fn load_file(path: &str) -> exec::FileLoadingFuture {
+    use std::cell::RefCell;
+    use std::rc::Rc;
+
+    let contents = Rc::new(RefCell::new(None));
+    let path = path.to_owned();
+
+    {
+        let contents = contents.clone();
+        let err_path = path.clone();
+
+        miniquad::fs::load_file(&path, move |bytes| {
+            *contents.borrow_mut() = Some(bytes.map_err(|kind| exec::FileError::new(kind, &err_path)));
+        });
+    }
+
+    exec::FileLoadingFuture { contents }
+}
