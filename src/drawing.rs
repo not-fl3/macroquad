e585bbccbaeca861686c898b81f2ab24eebe567c
Fedor Logachev <not.fl3@gmail.com>
2020-09-05 19:44:31 -0500

    core: Move code around, some cleaning up

--- src/drawing.rs
+++ src/drawing.rs
@@ -67,53 +67,13 @@ impl DrawContext {
         std::mem::swap(&mut ui_draw_list, &mut self.ui_draw_list);
     }
 
-    pub fn draw_rectangle(&mut self, x: f32, y: f32, w: f32, h: f32, color: Color) {
-        #[rustfmt::skip]
-        let vertices = [
-            Vertex::new(x    , y    , 0., 0.0, 1.0, color),
-            Vertex::new(x + w, y    , 0., 1.0, 0.0, color),
-            Vertex::new(x + w, y + h, 0., 1.0, 1.0, color),
-            Vertex::new(x    , y + h, 0., 0.0, 0.0, color),
-        ];
-        let indices: [u16; 6] = [0, 1, 2, 0, 2, 3];
-
-        self.gl.texture(None);
-        self.gl.draw_mode(DrawMode::Triangles);
-        self.gl.geometry(&vertices, &indices);
-    }
-
-    pub fn draw_line(&mut self, x1: f32, y1: f32, x2: f32, y2: f32, thickness: f32, color: Color) {
-        let dx = x2 - x1;
-        let dy = y2 - y1;
-        let nx = -dy; // https://stackoverflow.com/questions/1243614/how-do-i-calculate-the-normal-vector-of-a-line-segment
-        let ny = dx;
-
-        let tlen = (nx * nx + ny * ny).sqrt() / (thickness * 0.5);
-        if tlen < std::f32::EPSILON {
-            return;
-        }
-        let tx = nx / tlen;
-        let ty = ny / tlen;
-
-        self.gl.texture(None);
-        self.gl.draw_mode(DrawMode::Triangles);
-        self.gl.geometry(
-            &[
-                Vertex::new(x1 + tx, y1 + ty, 0., 0., 0., color),
-                Vertex::new(x1 - tx, y1 - ty, 0., 0., 0., color),
-                Vertex::new(x2 + tx, y2 + ty, 0., 0., 0., color),
-                Vertex::new(x2 - tx, y2 - ty, 0., 0., 0., color),
-            ],
-            &[0, 1, 2, 2, 1, 3],
-        );
-    }
 
     pub(crate) fn perform_render_passes(&mut self, ctx: &mut miniquad::Context) {
         self.draw_ui(ctx);
         self.gl.draw(ctx);
     }
 
-    pub fn update_projection_matrix(&mut self, ctx: &mut miniquad::Context) {
+    pub(crate) fn update_projection_matrix(&mut self, ctx: &mut miniquad::Context) {
         let (width, height) = ctx.screen_size();
 
         let mut projection = glam::Mat4::orthographic_rh_gl(0., width, height, 0., -1., 1.);
