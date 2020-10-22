8ed4816511183e8cf205ceebeca77e8ffc059154
Fedor Logachev <not.fl3@gmail.com>
2020-10-21 19:23:49 -0500

    megaui: Make megaui optional

--- src/drawing.rs
+++ src/drawing.rs
@@ -1,7 +1,5 @@
 //! this is legacy and going to disappear soon
 
-use std::collections::HashMap;
-
 use quad_gl::QuadGl;
 
 pub use quad_gl::{colors::*, Color, DrawMode, FilterMode, Image, Texture2D};
@@ -9,37 +7,16 @@ pub use quad_gl::{colors::*, Color, DrawMode, FilterMode, Image, Texture2D};
 use glam::Mat4;
 
 pub struct DrawContext {
-    pub(crate) font_texture: Texture2D,
     pub(crate) gl: QuadGl,
     pub(crate) camera_matrix: Option<Mat4>,
     pub(crate) current_pass: Option<miniquad::RenderPass>,
-    pub ui: megaui::Ui,
-
-    megaui_textures: HashMap<u32, Texture2D>,
-    ui_draw_list: Vec<megaui::DrawList>,
 }
 
 impl DrawContext {
     pub fn new(ctx: &mut miniquad::Context) -> DrawContext {
-        let mut ui = megaui::Ui::new();
-        ui.set_clipboard_object(crate::ui::ClipboardObject);
-
-        let texture_data = &ui.font_atlas.texture;
-        let font_texture = Texture2D::from_rgba8(
-            ctx,
-            texture_data.width as u16,
-            texture_data.height as u16,
-            &texture_data.data,
-        );
-        font_texture.set_filter(ctx, FilterMode::Nearest);
-
         let mut draw_context = DrawContext {
             camera_matrix: None,
             gl: QuadGl::new(ctx),
-            font_texture,
-            ui,
-            megaui_textures: HashMap::new(),
-            ui_draw_list: Vec::with_capacity(10000),
             current_pass: None,
         };
 
@@ -48,41 +25,11 @@ impl DrawContext {
         draw_context
     }
 
-    pub(crate) fn set_megaui_texture(&mut self, id: u32, texture: Texture2D) {
-        self.megaui_textures.insert(id, texture);
-    }
-
-    fn draw_ui(&mut self, _: &mut miniquad::Context) {
-        self.ui_draw_list.clear();
-
-        self.ui.render(&mut self.ui_draw_list);
-        let mut ui_draw_list = vec![];
-
-        std::mem::swap(&mut ui_draw_list, &mut self.ui_draw_list);
-        self.gl.texture(Some(self.font_texture));
-
-        for draw_command in &ui_draw_list {
-            if let Some(texture) = draw_command.texture {
-                self.gl.texture(Some(self.megaui_textures[&texture]));
-            } else {
-                self.gl.texture(Some(self.font_texture));
-            }
-            self.gl.scissor(
-                draw_command
-                    .clipping_zone
-                    .map(|rect| (rect.x as i32, rect.y as i32, rect.w as i32, rect.h as i32)),
-            );
-            self.gl.draw_mode(DrawMode::Triangles);
-            self.gl
-                .geometry(&draw_command.vertices, &draw_command.indices);
-        }
-        self.gl.texture(None);
-
-        std::mem::swap(&mut ui_draw_list, &mut self.ui_draw_list);
-    }
+    // pub(crate) fn set_megaui_texture(&mut self, id: u32, texture: Texture2D) {
+    //     self.megaui_textures.insert(id, texture);
+    // }
 
     pub(crate) fn perform_render_passes(&mut self, ctx: &mut miniquad::Context) {
-        self.draw_ui(ctx);
         self.gl.draw(ctx);
     }
 
