8ab743e86688e23722b5cbc89fec2e1bfbf80164
Fedor Logachev <not.fl3@gmail.com>
2020-11-03 18:55:18 -0600

    types/rect: Add small utilty functions for Rect

--- src/types.rs
+++ src/types.rs
@@ -1,6 +1,8 @@
 pub use quad_gl::Color;
 
-#[derive(Clone, Copy, Debug)]
+use glam::{vec2, Vec2};
+
+#[derive(Clone, Copy, Debug, Default)]
 pub struct Rect {
     pub x: f32,
     pub y: f32,
@@ -11,5 +13,50 @@ pub struct Rect {
 impl Rect {
     pub fn new(x: f32, y: f32, w: f32, h: f32) -> Rect {
         Rect { x, y, w, h }
+    }
+
+    pub fn point(&self) -> Vec2 {
+        vec2(self.x, self.y)
+    }
+
+    /// Returns the left edge of the `Rect`
+    pub fn left(&self) -> f32 {
+        self.x
+    }
+
+    /// Returns the right edge of the `Rect`
+    pub fn right(&self) -> f32 {
+        self.x + self.w
+    }
+
+    /// Returns the top edge of the `Rect`
+    pub fn top(&self) -> f32 {
+        self.y
+    }
+
+    /// Returns the bottom edge of the `Rect`
+    pub fn bottom(&self) -> f32 {
+        self.y + self.h
+    }
+
+    /// Moves the `Rect`'s origin to (x, y)
+    pub fn move_to(&mut self, destination: Vec2) {
+        self.x = destination.x();
+        self.y = destination.y();
+    }
+
+    /// Scales the `Rect` by a factor of (sx, sy),
+    /// growing towards the bottom-left
+    pub fn scale(&mut self, sx: f32, sy: f32) {
+        self.w *= sx;
+        self.h *= sy;
+    }
+
+    /// Checks whether the `Rect` contains a `Point`
+    pub fn contains(&self, point: Vec2) -> bool {
+        point.x() >= self.left()
+            && point.x() <= self.right()
+            && point.y() <= self.bottom()
+            && point.y() >= self.top()
     }
 }
