32bb775fefa50b8658ef5e872c0d505fb196d9ee
Joe Ardent <code@ardent.nebcorp.com>
2020-11-27 22:33:56 -0800

    fix the filepaths for images in examples

--- examples/texture.rs
+++ examples/texture.rs
@@ -2,7 +2,7 @@ use macroquad::prelude::*;
 
 #[macroquad::main("Texture")]
 async fn main() {
-    let texture: Texture2D = load_texture("ferris.png").await;
+    let texture: Texture2D = load_texture("examples/ferris.png").await;
 
     loop {
         clear_background(RED);
