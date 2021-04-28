23344859e1bad33d365024bc777b7a72114fab9a
Stephan Dilly <dilly.stephan@gmail.com>
2021-04-29 00:34:41 +0200

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -91,7 +91,7 @@ use ui::ui_context::UiContext;
 
 struct Context {
     quad_context: QuadContext,
-    #[cfg(feature="audio")]
+    #[cfg(feature = "audio")]
     audio_context: audio::AudioContext,
 
     screen_width: f32,
@@ -225,7 +225,7 @@ impl Context {
             fonts_storage: text::FontsStorage::new(&mut ctx),
 
             quad_context: ctx,
-            #[cfg(feature="audio")]
+            #[cfg(feature = "audio")]
             audio_context: audio::AudioContext::new(),
             coroutines_context: experimental::coroutines::CoroutinesContext::new(),
 
