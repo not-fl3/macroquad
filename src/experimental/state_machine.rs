0855d51de48f796fa839cc634882bb63bf8ee629
Fedor Logachev <not.fl3@gmail.com>
2021-09-07 11:40:51 -0500

    state_machine: Manual poll coroutines
    state_machine's update was decoupled with coroutines update
    
    So it was impossible to stop the state machine or just not update it every frame
    
    After this change state_machine's coroutines are not doing anything without .update() and update's delta time applies to coroutines

--- src/experimental/state_machine.rs
+++ src/experimental/state_machine.rs
@@ -1,10 +1,12 @@
 use crate::experimental::coroutines::Coroutine;
 
-type UpdateFn<T> = Box<dyn FnMut(&mut T, f32)>;
-type CoroutineFn<T> = Box<dyn FnMut(&mut T) -> Coroutine>;
-type OnEndFn<T> = Box<dyn FnMut(&mut T)>;
+use crate::experimental::scene;
 
-pub struct State<T> {
+type UpdateFn<T> = Box<dyn FnMut(&mut scene::RefMut<T>, f32)>;
+type CoroutineFn<T> = Box<dyn FnMut(&mut scene::RefMut<T>) -> Coroutine>;
+type OnEndFn<T> = Box<dyn FnMut(&mut scene::RefMut<T>)>;
+
+pub struct State<T: 'static> {
     update: Option<UpdateFn<T>>,
     coroutine: Option<CoroutineFn<T>>,
     on_end: Option<OnEndFn<T>>,
@@ -19,21 +21,24 @@ impl<T> State<T> {
         }
     }
 
-    pub fn update(self, update: impl FnMut(&mut T, f32) + 'static) -> Self {
+    pub fn update(self, update: impl FnMut(&mut scene::RefMut<T>, f32) + 'static) -> Self {
         State {
             update: Some(Box::new(update)),
             ..self
         }
     }
 
-    pub fn coroutine(self, coroutine: impl FnMut(&mut T) -> Coroutine + 'static) -> Self {
+    pub fn coroutine(
+        self,
+        coroutine: impl FnMut(&mut scene::RefMut<T>) -> Coroutine + 'static,
+    ) -> Self {
         State {
             coroutine: Some(Box::new(coroutine)),
             ..self
         }
     }
 
-    pub fn on_end(self, on_end: impl FnMut(&mut T) + 'static) -> Self {
+    pub fn on_end(self, on_end: impl FnMut(&mut scene::RefMut<T>) + 'static) -> Self {
         State {
             on_end: Some(Box::new(on_end)),
             ..self
@@ -41,7 +46,7 @@ impl<T> State<T> {
     }
 }
 
-pub enum StateMachine<T> {
+pub enum StateMachine<T: 'static> {
     Ready(StateMachineOwned<T>),
     InUse {
         next_state: Option<usize>,
@@ -49,7 +54,7 @@ pub enum StateMachine<T> {
     },
 }
 
-impl<T: 'static> StateMachine<T> {
+impl<T: scene::Node + 'static> StateMachine<T> {
     pub fn new() -> StateMachine<T> {
         StateMachine::Ready(StateMachineOwned::new())
     }
@@ -110,13 +115,27 @@ impl<T: 'static> StateMachine<T> {
     }
 
     /// A hack to update a state machine being part of an updating struct
-    pub fn update_detached<'a, F: FnMut(&mut T) -> &mut StateMachine<T>>(t: &'a mut T, mut f: F) {
-        let mut state_machine = f(t).take();
-        state_machine.update(t, crate::time::get_frame_time());
-        f(t).put_back(state_machine);
+    pub fn update_detached<'a, F>(mut t: scene::RefMut<T>, mut f: F)
+    where
+        F: FnMut(&mut scene::RefMut<T>) -> &mut StateMachine<T>,
+    {
+        let mut state_machine = f(&mut t).take();
+        state_machine.update(&mut t, crate::time::get_frame_time());
+
+        // coroutine may want to access this node by its handle
+        // but while we have borrowed t to RefMut - it will fail this attempt
+        // so here we drop t, poll the coroutine and than getting t back by its handle
+        let handle = t.handle();
+        drop(t);
+        if let Some(ref mut coroutine) = state_machine.active_coroutine {
+            coroutine.poll(crate::time::get_frame_time() as _);
+        }
+        let mut t = crate::experimental::scene::get_node(handle);
+
+        f(&mut t).put_back(state_machine);
     }
 
-    pub fn update<'a>(&mut self, t: &'a mut T) {
+    pub fn update<'a>(&mut self, t: &'a mut scene::RefMut<T>) {
         match self {
             StateMachine::Ready(state_machine) => {
                 state_machine.update(t, crate::time::get_frame_time())
@@ -126,8 +145,9 @@ impl<T: 'static> StateMachine<T> {
     }
 }
 
-pub struct StateMachineOwned<T> {
+pub struct StateMachineOwned<T: 'static> {
     states: Vec<State<T>>,
+    active_coroutine: Option<Coroutine>,
     next_state: Option<usize>,
     current_state: usize,
 }
@@ -142,6 +162,7 @@ impl<T: 'static> StateMachineOwned<T> {
         }
         StateMachineOwned {
             states,
+            active_coroutine: None,
             next_state: None,
             current_state: 0,
         }
@@ -161,19 +182,22 @@ impl<T: 'static> StateMachineOwned<T> {
         self.current_state
     }
 
-    fn update(&mut self, player: &mut T, dt: f32) {
+    fn update(&mut self, player: &mut scene::RefMut<T>, dt: f32) {
         if let Some(next_state) = self.next_state {
             if next_state != self.current_state {
                 if let Some(on_end) = &mut self.states[self.current_state].on_end {
                     on_end(player);
                 }
                 if let Some(coroutine) = &mut self.states[next_state].coroutine {
-                    coroutine(player);
+                    let mut coroutine = coroutine(player);
+                    coroutine.set_manual_poll();
+                    self.active_coroutine = Some(coroutine);
                 }
             }
             self.current_state = next_state;
             self.next_state = None;
         }
+
         if let Some(update) = self.states[self.current_state].update.as_mut() {
             (update)(player, dt);
         }
