c7f9aa2832d9c5ef59204d78b400d7cb929bad8e
Fedor Logachev <not.fl3@gmail.com>
2021-09-13 18:19:05 -0500

    ui: Fix warning in ui.rs
    removed unused Window field, "visible"

--- src/ui.rs
+++ src/ui.rs
@@ -106,7 +106,6 @@ impl From<crate::texture::Texture2D> for UiContent<'static> {
 pub(crate) struct Window {
     pub id: Id,
     pub parent: Option<Id>,
-    pub visible: bool,
     // active is set to true when the begin_window is called on this window
     // and is going to be set to false at the end of each frame
     pub active: bool,
@@ -145,7 +144,6 @@ impl Window {
             vertical_scroll_bar_width: 0.,
             title_height,
             parent,
-            visible: true,
             was_active: false,
             active: false,
             painter: Painter::new(atlas),
