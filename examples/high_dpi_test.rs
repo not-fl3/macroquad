49a144ec9f0a228ad7abc3e1873e54f6a9d7d4e2
LimeShulkerBox <77601849+piemanau@users.noreply.github.com>
2023-01-31 16:28:31 +1100

    Fix example
    Modify one of the DPI examples to show the same as the other DPI example

--- examples/high_dpi_test.rs
+++ examples/high_dpi_test.rs
@@ -8,9 +8,9 @@ async fn main() {
         set_default_camera();
 
         //Draw some rectangles to demonstrate that they look the same on both high DPI and low DPI screens
-        draw_rectangle(0f32, 0f32, screen_width(), screen_height(), RED);
-        draw_rectangle(0f32, 0f32, screen_width() / 2., screen_height() / 2., GREEN);
-        
+        draw_rectangle(100f32, 100f32, screen_width() - 200., screen_height() - 200., RED);
+        draw_rectangle(100f32, 100f32 + (screen_height() - 200.) / 2., (screen_width() - 200.) / 2., (screen_height() - 200.) / 2., GREEN);
+
         next_frame().await;
     }
 }
