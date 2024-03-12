ba37f9537faf19f415ce97849971c2350b8aeeca
Alan Klas <alan@alanklas.com>
2024-03-12 23:17:20 +0100

    no idea why this is broken

--- src/lib.rs
+++ src/lib.rs
@@ -378,7 +378,7 @@ impl Context {
         self.ui_context.process_input();
 
         // Check if the window conf "clear_background" is set to false
-        if miniquad::window::get_clear_background() {
+        if miniquad::window::clear_background() {
             let color = Self::DEFAULT_BG_COLOR;
             get_quad_context().clear(Some((color.r, color.g, color.b, color.a)), None, None);
             self.gl.reset();
